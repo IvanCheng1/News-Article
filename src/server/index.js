749c6e429a86a42a16ad2d8aecc72c94e909a386
Ivan Cheng <ivan@ivancheng.co.uk>
2020-06-17 15:30:26 +0100

    installed babel

--- src/server/index.js
+++ src/server/index.js
@@ -9,6 +9,7 @@ var textapi = new aylien({
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const PORT = process.env.PORT || 8080
 
 var aylien = require("aylien_textapi")
 
@@ -20,12 +21,12 @@ console.log(__dirname)
 
 app.get('/', function(req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile(path.resolve('dist/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function() {
-    console.log('Example app listening on port 8080!')
+app.listen(PORT, function() {
+    console.log(`Example app listening on port ${PORT}!`)
 })
 
 app.get('/test', function(req, res) {
