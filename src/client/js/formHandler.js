c4140575d535939a6d20c01ba1ad8b58749d0d53
Ivan Cheng <ivan@ivancheng.co.uk>
2020-06-18 16:24:20 +0100

    scss

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -10,6 +10,8 @@ function handleSubmit(event) {
         alert("Please enter a valid URL")
         return
     }
+    // spinning wheel
+    document.getElementById('loader').style.display = 'block'
     const url = 'http://localhost:8080/api'
     const data = postData(url, formText)
         .then((data) => {
@@ -29,25 +31,35 @@ const postData = async(url, data) => {
             data: data
         })
     })
+
     return response.json();
 }
 
 const updateUI = async(data) => {
     if (Object.keys(data).length === 0) {
         alert('Page not found')
+        document.getElementById('loader').style.display = 'none'
         return
     }
     for (let [key, value] of Object.entries(data)) {
+        if (key.includes('confidence')) {
+            // change float to percentage
+            value = (Math.round(value * 100)).toString() + '%'
+        } else if (key === 'text') {
+            // add line breaks
+            value = value.replace(/\n/g, '<br>\n')
+        }
         document.getElementById(key).innerHTML = value
     }
+    document.getElementById('container').classList.remove('hide')
+    document.getElementById('container').classList.add('show')
+    document.getElementById('loader').style.display = 'none'
+    document.getElementById('form').reset()
 }
 
 function clearResults() {
-    document.getElementById('polarity').innerHTML = ''
-    document.getElementById('subjectivity').innerHTML = ''
-    document.getElementById('text').innerHTML = ''
-    document.getElementById('polarity_confidence').innerHTML = ''
-    document.getElementById('subjectivity_confidence').innerHTML = ''
+    document.getElementById('container').classList.add('hide')
+    document.getElementById('container').classList.remove('show')
 }
 
 export { handleSubmit }
